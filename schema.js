4541b60814f580ffee21ed47903efa53efbad2f2
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2020-02-17 06:08:14 -0300

    hardcoded data

---
+++ schema.js
@@ -0,0 +1,54 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = require("graphql");
+
+//Hardcoded data
+const customers = [
+  { id: "1", name: "John Doe", email: "jdoe@gmail.com", age: 35 },
+  { id: "2", name: "Steve Smith", email: "steve@gmail.com", age: 25 },
+  { id: "3", name: "Sara Williams", email: "sara@gmail.com", age: 32 }
+];
+
+const CustomerType = new GraphQLObjectType({
+  name: "Customer",
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id == args.id) {
+            return customers[i];
+          }
+        }
+      }
+    },
+    customers: {
+      type: new GraphQLList(CustomerType),
+      resolve(parentValue, args) {
+        return customers;
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
